a8f939621a66025b9530b0fb542e3550804d398f
Daniel William Rutter <danielrutter@privatesaver.com>
2018-11-01 16:00:49 +0000

    RE-UPLOADED The Entire Project So You Can Download VIA Git The Project Than Having To Use An Outdated ZIP File System ETC.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+<title>Basic Fixed Width Navbar - 25th February 2017</title>
+<link rel="stylesheet" type="text/css" href="/css/global/main.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+<!-- File Location: /index.php -->
+
+<br>
+
+<nav>
+  <ul>
+    <li><a href="#" onclick="return false">LINK 1</a></li>
+    <li><a href="#" onclick="return false">LINK 2</a></li>
+    <li><a href="#" onclick="return false">LINK 3</a></li>
+    <li><a href="#" onclick="return false">LINK 4</a></li>
+    <li><a href="#" onclick="return false">LINK 5</a></li>
+    <li><a href="#" onclick="return false">LINK 6</a></li>
+    <li><a href="#" onclick="return false">LINK 7</a></li>
+    <li><a href="#" onclick="return false">LINK 8</a></li>
+  </ul> 
+</nav>
+
+<br>
+
+</body>
+</html>
